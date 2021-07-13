06094be556a5bcec28a49ba4129220c2544285ed
Yalishandela <aleksandra.a.salamon@gmail.com>
2021-07-13 13:13:33 +0200

    Add new files

---
+++ Calc/Calculator.cpp
@@ -0,0 +1,22 @@
+class Calculator
+{
+public:
+	double Calculate(double x, char oper, double y);
+};
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+	switch (oper)
+	{
+	case '+':
+		return x + y;
+	case '-':
+		return x - y;
+	case '*':
+		return x * y;
+	case '/':
+		return x / y;
+	default:
+		return 0;
+	}
+}
